f5b5751f49b6eb4ebc3b91597f97b1c3a4c43d70
nazmul5297 <nazmulhaque5297@outlook.com>
2023-03-12 18:02:30 +0600

    add ado

--- CrudWithAspNet/Controllers/AdmitedStudentInfoController.cs
+++ CrudWithAspNet/Controllers/AdmitedStudentInfoController.cs
@@ -1,11 +1,14 @@
 ﻿using CrudWithAspNet.Context;
 using CrudWithAspNet.Model;
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Runtime.Intrinsics.Arm;
 
 namespace CrudWithAspNet.Controllers
 {
-    [Route("action")]
+   
     [ApiController]
     public class AdmitedStudentInfoController : ControllerBase
     {
@@ -16,11 +19,37 @@ namespace CrudWithAspNet.Controllers
         }
 
 
-        [HttpGet]
+        [HttpGet("getAdmittedStudentInfo")]
         public List<Admited_Student_Info> GelAllData()
         {
-            var admitedStudentInfo = _dbContext.admited_Student_Infos.ToList();
+            List<Admited_Student_Info> admitedStudentInfo = new List<Admited_Student_Info>();
+            SqlConnection con = new SqlConnection("Server=laptop-eoe2lieg;Database=School_Management;Trusted_Connection=True;TrustServerCertificate=True;");
+            SqlCommand cmd = new SqlCommand("select * from admited_Student_Infos", con);
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                Admited_Student_Info student = new Admited_Student_Info();
+                student.Id = Convert.ToInt32(rdr["Id"]);
+                student.Father_Name = rdr["Father_Name"].ToString();
+                admitedStudentInfo.Add(student);
+            }
+            con.Close();
             return admitedStudentInfo;
         }
+
+
+
+        [HttpPost("addStudent")]
+        public Admited_Student_Info Add(Admited_Student_Info student_Info)
+        {
+            student_Info.CreatedDate = DateTime.Now;
+            _dbContext.admited_Student_Infos.Add(student_Info);
+            bool isSaved = _dbContext.SaveChanges() > 0;
+            return isSaved ? student_Info : null;
+
+        }
+
     }
 }
